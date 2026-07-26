72949f07e220f9b4adf9e9ebbacf9a459c45bf6c
Tasuku <07tasuku06@gmail.com>
2026-07-26 21:21:58 +0900

    feat: 書籍管理のデータベース設計を実装

---
+++ app/Models/Book.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'author',
+        'isbn',
+        'published_date',
+        'description',
+        'image_url',
+    ];
+
+    protected $casts = [
+        'published_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class)->withTimestamps();
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function favoritedUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorites')
+            ->withTimestamps();
+    }
+}
