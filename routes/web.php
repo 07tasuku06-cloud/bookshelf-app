45bd2ee46d6da8b91a0d33aa43d9e0e22e8e2243
Tasuku <07tasuku06@gmail.com>
2026-07-29 10:29:39 +0900

    feat: 各種一覧取得と基本ルーティングを実装

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\RankingController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// トップページ・書籍一覧・詳細は誰でも閲覧可能
+Route::get('/', [BookController::class, 'index'])->name('home');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
 
-Route::get('/', function () {
-    return view('welcome');
+// ログイン必須
+Route::middleware('auth')->group(function () {
+    Route::resource('genres', GenreController::class);
+    Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 });
+
+// 動的URLはcreateより後に書く
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+
+Route::get('/ranking', [RankingController::class, 'index'])
+    ->name('ranking.index');
